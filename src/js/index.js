c088f692506c83f266c63bdae2f92743165dff6b
Rob Laughlin <waterfallkicks@gmail.com>
2025-01-03 14:21:54 -0800

    Implement search bar functionality for weather

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
 import { getTimeline } from "./WeatherApi";
+import "../css/index.css";
 
-async function init() {
+async function weatherSearchBtnClicked(event) {
+    const form = event.target.parentElement;
+    const searchBar = form.querySelector("#WeatherSearchBar");
+    const query = searchBar.value;
+
+    if (query === "") {
+        alert("Search bar must be non-empty.");
+        return;
+    }
+
     try {
-        const timeline = await getTimeline("new york city");
+        const timeline = await getTimeline(query);
         console.log(timeline);
     } catch (err) {
         console.log(err);
     }
 }
 
-init();
+let weatherSearchBtn = document.querySelector("#SearchForm > button");
+weatherSearchBtn.addEventListener("click", weatherSearchBtnClicked);
